60a940a71ba0fec5190fecec152f999f66f03856
Анастасия <eugengamilov@gmail.com>
2020-06-14 20:55:38 +0300

    Updates

---
+++ precache-manifest.cb6216f2e02ae40189b2d74cd7a724d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b02d1b38897cce64403d5c0ae2c24411",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "f8af4337b1ca491bddba",
+    "url": "/goit-react-hw-01-components/static/css/main.a679347c.chunk.css"
+  },
+  {
+    "revision": "25fb5120d9d7778da26b",
+    "url": "/goit-react-hw-01-components/static/js/2.97b0eebc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.97b0eebc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8af4337b1ca491bddba",
+    "url": "/goit-react-hw-01-components/static/js/main.db7ec4fd.chunk.js"
+  },
+  {
+    "revision": "b61ca29d0eb33ab534b8",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.1ef4f175.js"
+  }
+]);
